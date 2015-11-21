9bf4d9da22e876e219662c31320ce56df6d40a17
Juan Pablo Scaletti <juanpablo@lucumalabs.com>
2015-11-21 08:34:45 -0500

    Fix the utils._is_sequence function for py3.*
    Apparently the evaluation order of an `A and B or C` expression has
    changed between versions. Better to use parenthesis to make the intent
    clear

--- allspeak/utils.py
+++ allspeak/utils.py
@@ -103,8 +103,6 @@ def _flatten(dic):
 
 
 def _is_sequence(arg):
-    return (
-        not hasattr(arg, "strip") and
-        hasattr(arg, "__getitem__") or
-        hasattr(arg, "__iter__")
+    return not hasattr(arg, "strip") and (
+        hasattr(arg, "__getitem__") or hasattr(arg, "__iter__")
     )
