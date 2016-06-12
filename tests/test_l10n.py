37e0a312368f380a55340909dff8f04f95cc84ca
Juan Pablo Scaletti <juanpablo@lucumalabs.com>
2016-06-12 16:16:10 -0500

    tests

--- tests/test_l10n.py
+++ tests/test_l10n.py
@@ -183,7 +183,7 @@ def test_format_currency():
     assert (l10n.format_currency(1099.98, 'USD', locale='en_US') ==
             u'$1,099.98')
     assert (l10n.format_currency(1099.98, 'USD', locale='es_CO') ==
-            u'1.099,98\xa0US$')
+            u'US$\xa01.099,98')
     assert (l10n.format_currency(1099.98, 'EUR', locale='de_DE') ==
             u'1.099,98\xa0\u20ac')
     assert (l10n.format_currency(1099.98, 'EUR', u'\xa4\xa4 #,##0.00', locale='en_US') ==
